f5e06ed30eb2553fc8b8e6a5ed9c3912e50437b6
Serafim <serafim@fachiniit.com>
2024-10-01 17:12:04 -0300

    Versão final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,21 +26,134 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="pt-16 h-screen w-full bg-gray-200  flex items-center">
-        <div className="flex flex-col w-96 mx-auto text-center gap-4">
+      <div className="py-16 h-full w-full bg-gray-300 flex items-center">
+        <div className="flex flex-col w-full max-w-2xl mx-auto text-center gap-4">
           <h2 className="text-3xl">Produtos</h2>
           <p>
             Encontre o estilo que combina com você
           </p>
-          <div className="grid grid-cols-4">
-            <img src="/camisas/camisa1.png" />
-            <img src="/camisas/camisa2.png" />
-            <img src="/camisas/camisa3.png" />
-            <img src="/camisas/camisa4.png" />
-            <img src="/camisas/camisa5.png" />
-            <img src="/camisas/camisa6.png" />
-            <img src="/camisas/camisa7.png" />
-            <img src="/camisas/camisa1.png" />
+          <div className="grid grid-cols-4 gap-4">
+            <div className="p-2 rounded shadow bg-white">
+              <img className="w-60 h-30 object-cover" src="/camisas/camisa1.png" />
+              <div className="flex flex-col text-sm text-start">
+                <p>
+                  Nome do produto
+                </p>
+                <p className="ml-auto text-green-300">
+                  R$ 100,00
+                </p>
+              </div>
+            </div> 
+            <div className="p-2 rounded shadow bg-white">
+              <img className="w-60 h-30 object-cover" src="/camisas/camisa2.png" />
+              <div className="flex flex-col text-sm text-start">
+                <p>
+                  Nome do produto
+                </p>
+                <p className="ml-auto text-green-300">
+                  R$ 100,00
+                </p>
+              </div>
+            </div> 
+            <div className="p-2 rounded shadow bg-white">
+              <img className="w-60 h-30 object-cover" src="/camisas/camisa3.png" />
+              <div className="flex flex-col text-sm text-start">
+                <p>
+                  Nome do produto
+                </p>
+                <p className="ml-auto text-green-300">
+                  R$ 100,00
+                </p>
+              </div>
+            </div> 
+            <div className="p-2 rounded shadow bg-white">
+              <img className="w-60 h-30 object-cover" src="/camisas/camisa4.png" />
+              <div className="flex flex-col text-sm text-start">
+                <p>
+                  Nome do produto
+                </p>
+                <p className="ml-auto text-green-300">
+                  R$ 100,00
+                </p>
+              </div>
+            </div> 
+            <div className="p-2 rounded shadow bg-white">
+              <img className="w-60 h-30 object-cover" src="/camisas/camisa5.png" />
+              <div className="flex flex-col text-sm text-start">
+                <p>
+                  Nome do produto
+                </p>
+                <p className="ml-auto text-green-300">
+                  R$ 100,00
+                </p>
+              </div>
+            </div> 
+            <div className="p-2 rounded shadow bg-white">
+              <img className="w-60 h-30 object-cover" src="/camisas/camisa6.png" />
+              <div className="flex flex-col text-sm text-start">
+                <p>
+                  Nome do produto
+                </p>
+                <p className="ml-auto text-green-300">
+                  R$ 100,00
+                </p>
+              </div>
+            </div> 
+            <div className="p-2 rounded shadow bg-white">
+              <img className="w-60 h-30 object-cover" src="/camisas/camisa7.png" />
+              <div className="flex flex-col text-sm text-start">
+                <p>
+                  Nome do produto
+                </p>
+                <p className="ml-auto text-green-300">
+                  R$ 100,00
+                </p>
+              </div>
+            </div> 
+            <div className="p-2 rounded shadow bg-white">
+              <img className="w-60 h-30 object-cover" src="/camisas/camisa1.png" />
+              <div className="flex flex-col text-sm text-start">
+                <p>
+                  Nome do produto
+                </p>
+                <p className="ml-auto text-green-300">
+                  R$ 100,00
+                </p>
+              </div>
+            </div> 
+          </div>
+        </div>
+      </div>
+      <div className="w-full bg-black text-white text-xs p-10">
+        <div className="w-full h-[1px] bg-white"></div>
+        <div className="flex justify-between">
+          <div className="flex flex-col">
+            <img className="w-40 h-20 object-contain" src="images/Logo.png" alt="" />
+            <p>
+              Sua camiseta mais confortavel, feita para seu estilo e seu conforto.
+            </p>
+          </div>
+          <div className="flex pt-10 flex-col gap-4">
+            <div className="flex gap-8 text-green-700">
+              <p>Discovery</p>
+              <p>Sobre</p>
+              <p>Contate-nos</p>
+            </div>
+            <div className="flex gap-8 ">
+              <p>Nova temporada</p>
+              <p>Ajuda</p>
+              <p>Contate-nos</p>
+            </div>
+            <div className="flex gap-8 ">
+              <p>Mais pesquisado</p>
+              <p>Envio</p>
+              <p>Políticas de Privacidade</p>
+            </div>
+            <div className="flex gap-8 ">
+              <p>Mais vendido</p>
+              <p>Afiliado</p>
+              <p>Termos e Condições</p>
+            </div>
           </div>
         </div>
       </div>
