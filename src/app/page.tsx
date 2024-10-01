8f82aa82fe1a1494beba92ee19809e4b19963ada
Serafim <serafim@fachiniit.com>
2024-10-01 16:36:31 -0300

    Novo icone e navbar pronta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 export default function Home() {
   return (
-    <div>
-      <div className="h-16 bg-black w-screen absolute text-white flex flex-row justify-between items-center">
-        <img src="/images/Logo.png" className="h-12"  />
-        <div className="flex gap-16">
-          <p>Discovery</p>
-          <p>Sobre</p>
-          <p>Contate-nos</p>
-        </div>
-        <div className="flex">
+    <div className="w-full h-full">
+      <div className="h-16 bg-black w-full absolute text-white flex items-center justify-center overflow-x-hidden">
+        <div className="flex flex-row justify-between items-center w-full max-w-[768px]">
+          <img src="/images/Logo.png" className="h-12"/>
+          <div className="flex gap-16">
+            <p>Discovery</p>
+            <p>Sobre</p>
+            <p>Contate-nos</p>
+          </div>
+          <div className="flex gap-2">
+            <img src="/icons/user-icon.svg" className="w-6 h-6" alt="" />
+            <img src="/icons/cart-shopping.svg" className="w-6 h-6" alt="" />
+          </div>
         </div>
       </div>
       <div className="pt-16 h-screen bg-gray-800 text-white flex items-center">
@@ -19,6 +23,24 @@ export default function Home() {
           </p>
           <div className="bg-green-300 rounded px-4 py-1 cursor-pointer font-bold">
             Descubra nossa coleção
+          </div>
+        </div>
+      </div>
+      <div className="pt-16 h-screen w-full bg-gray-200  flex items-center">
+        <div className="flex flex-col w-96 mx-auto text-center gap-4">
+          <h2 className="text-3xl">Produtos</h2>
+          <p>
+            Encontre o estilo que combina com você
+          </p>
+          <div className="grid grid-cols-4">
+            <img src="/camisas/camisa1.png" />
+            <img src="/camisas/camisa2.png" />
+            <img src="/camisas/camisa3.png" />
+            <img src="/camisas/camisa4.png" />
+            <img src="/camisas/camisa5.png" />
+            <img src="/camisas/camisa6.png" />
+            <img src="/camisas/camisa7.png" />
+            <img src="/camisas/camisa1.png" />
           </div>
         </div>
       </div>
